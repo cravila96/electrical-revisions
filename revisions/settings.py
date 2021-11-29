056b17f8ed85a6c10e7f4d46c1a7d999c83cacfd
Cristian Avila <ceavila96@gmail.com>
2021-11-28 22:11:56 -0500

    project instanced successfully

--- revisions/settings.py
+++ revisions/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'revisions.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prueba',
+        'HOST': 'db-app-electricidad.c9jiqxqgde1k.us-east-2.rds.amazonaws.com',
+        'USER': 'admin',
+        'PASSWORD': 'Woden2021*',
+        'PORT': '3306',
     }
 }
 
